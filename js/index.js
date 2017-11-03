c07b984b9c7bbf083babd9ea5a26e68106b38c6f
Rogério Moreira <r@rgllm.com>
2017-11-03 16:13:43 +0000

    samuel update

--- js/index.js
+++ js/index.js
@@ -4,24 +4,28 @@
       var count, myExp, output, searchField;
       count = void 0;
       myExp = void 0;
-      output = void 0;
+      output = "";
       searchField = void 0;
       searchField = $('#jsonSearch').val();
-      tokens = searchField.split(" ");
+      //tokens = searchField.split(" ");
+      
       myExp = new RegExp(searchField, 'i');
-      output = '<div class="row">';
       count = 1;
       $.getJSON('data/docentes.json', function(data) {
         $.each(data, function(key, val) {
-          if (val.nome.search(myExp) != -1 || val.email.search(myExp) !=-1) {
+          var showName = true
+          var showMail = true
+          searchField.split(" ").forEach(function(x) {
+            showName = showName && (val.nome.search(new RegExp(x, 'i'))!=-1);
+            showMail = showMail && (val.email.search(new RegExp(x, 'i'))!=-1);
+          });
+          if (showName || showMail) {
             output += '<div class="col s12 result card-panel"><div class="col s12"><li class="searchLi light-blue-text"><p>Nome: ' + val.nome + '</p><p>Website: <a href="' + val.url + '">'+val.url+'</a></p><p>Departamento: ' + val.departamento + '</p><p>Telefone: '+ val.telefone + '</p><p>Email: ' + val.email +'</p></li></div></div>';
-            if (count % 2 == 0) {
-              output += '</div><div class="row">';
-            }
+            
             count++;
           }
         });
-        output += '</div>';
+        output="<p>"+(count-1)+" resultados</p>"+output;
         $('#jsonSearchresults').html(output);
       });
     });
